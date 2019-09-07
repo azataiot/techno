1d85a738bb634b1314878f7c8bb587044c8f4a7a
Yaakov Azat <yaakovazat@gmail.com>
2019-09-08 01:54:39 +0600

    update shell again

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ else
     python3 -m venv venv
     source venv/bin/activate
     echo 'virtualenv activated!'
-    python3 ./azt/techno/si1145/setup.py install --prefix=$HOME/.local
+    python3 ./azt/techno/si1145/setup.py install
     echo 'SI45 drivers installed successfully!'
     echo 'Done!'
 fi
